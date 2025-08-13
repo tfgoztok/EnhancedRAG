38442349b912dfab0530ca1b1be969612fc4ebe7
Taner Furkan Goztok <tfgoztok@Taners-MacBook-Pro.local>
2025-08-12 22:36:17 -0500

    Initial commit

---
+++ src/main/java/cs544/project/EnhancedRAG/EnhancedRagApplication.java
@@ -0,0 +1,13 @@
+package cs544.project.EnhancedRAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhancedRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhancedRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs544/project/EnhancedRAG/EnhancedRagApplicationTests.java
@@ -0,0 +1,13 @@
+package cs544.project.EnhancedRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancedRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
